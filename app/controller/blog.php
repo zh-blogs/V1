d6b7731a3b86380b5c017ec9eb420e437d2c3cc4
soxft <email@xcsoft.top>
2022-04-02 19:03:10 +0800

    add: blog list complete

--- app/controller/blog.php
+++ app/controller/blog.php
@@ -2,6 +2,7 @@
 
 namespace app\controller;
 
+use common\BlogHelper;
 use Webman\Http\Request;
 use Webman\Http\Response;
 use Support\Db;
@@ -17,12 +18,34 @@ class Blog
      */
     public function list(Request $request): Response
     {
-        $page  = $request->input('page', 1);  // 每页显示条数
-        $limit = $request->input('limit', 10);  // 偏移量
+        $page   = $request->input('page', 1);  // 每页显示条数
+        $limit  = $request->input('limit', 10);  // 偏移量
         $search = $request->input('search', ''); // 搜索关键字
 
-        $data = Db::table('blogs')->select('*')->forPage($page, $limit)->whereRaw("MATCH (hobbies) AGAINST ('soccer' IN BOOLEAN MODE)")->get();
+        // get count
+        $countSql = Db::table('blogs');
+        if ($search !== '') {
+            $countSql = $countSql->where('name', 'like', '%' . $search . '%')
+                ->orWhere('url', 'like', '%' . $search . '%');
+        }
+        $count = $countSql->count();
 
-        return api(data: $data);
+        // get data
+        $sql = Db::table('blogs')->select('*')->forPage($page, $limit);
+        if ($search !== '') {
+            $sql = $sql->where('name', 'like', '%' . $search . '%')
+                ->orWhere('url', 'like', '%' . $search . '%');
+        }
+        $data = $sql->get();
+
+        // getTag
+        foreach ($data as &$item) {
+            $item->tags = implode(',', BlogHelper::getTagByBlogId($item->id));
+        }
+
+        return api(data: [
+            'data' => $data,
+            'count' => $count
+        ]);
     }
 }
---
+++ common/BlogHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace common;
+
+use support\Db;
+
+class BlogHelper
+{
+    /**
+     * 通过 blogId 获取 tag 数组
+     *
+     * @param integer $blogId
+     * @return array
+     */
+    public static function getTagByBlogId(int $blogId): array
+    {
+        $tag_ids = Db::table('tags')->select('tag_id')->where('blog_id', $blogId)->get();
+        if (!$tag_ids) return [];
+
+        foreach ($tag_ids as $k => $item) {
+            $tags[$k] = self::getTagNameByTagId($item->tag_id);
+        }
+
+        return $tags;
+    }
+
+    /**
+     * 通过 tagId 获取 tagName
+     *
+     * @param integer $tagId
+     * @return string
+     */
+    public static function getTagNameByTagId(int $tagId): string
+    {
+
+        $s = Db::table('tag_map')->select('name')->where('id', $tagId)->first();
+        if (!$s->name) return '';
+        return $s->name;
+    }
+}
