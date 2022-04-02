4e70e4041872a9c62801a82396545f6acdd28100
soxft <email@xcsoft.top>
2022-04-02 17:17:30 +0800

    fix: edit 404 route

--- config/route.php
+++ config/route.php
@@ -9,9 +9,5 @@ Route::fallback(function (Request $request) {
     return api(
         404,
         'Not Found',
-        [
-            'url' => $request->url(),
-            'method' => $request->method(),
-        ]
     )->withHeader('Access-Control-Allow-Origin', '*')->withStatus(404);
 });
