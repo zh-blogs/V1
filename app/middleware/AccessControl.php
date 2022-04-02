e18f87d0e12f2f7ef6644083eae701cf75e5f341
soxft <email@xcsoft.top>
2022-04-02 17:10:34 +0800

    fix: option request

--- app/middleware/AccessControl.php
+++ app/middleware/AccessControl.php
@@ -13,12 +13,12 @@ class AccessControl implements MiddlewareInterface
 {
     public function process(Request $request, callable $next): Response
     {
-        $response = $request->method() == 'OPTIONS' ? api(-1, 'permission deniend') : $next($request);
+        $response = $request->method() == 'OPTIONS' ? new Response(204, [], '') : $next($request);
 
         $response->withHeaders([
             'Access-Control-Allow-Origin' => '*',
             'Content-Type' => "Application/json; charset=utf-8",
-            'Server' => 'xcsoft'
+            'Server' => 'zhblog'
         ]);
 
         return $response;
